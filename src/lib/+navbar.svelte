2555d2e507d6bb375b191cd80ca81b0b9c57573a
Hizola, John Carlo <157343031+hizzCarlo@users.noreply.github.com>
2024-11-11 12:55:42 +0800

    Update +navbar.svelte

--- src/lib/+navbar.svelte
+++ src/lib/+navbar.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div class="w-full flex px-10 justify-between items-center  bg-slate-300 h-20 shadow-md absolute top-0 z-10">
-    <a href="/">
+    <a href="/project2/">
         <p class=" drop-shadow-md font-bold text-4xl text-slate-100"><span class=" text-yellow-300">TASTE</span> HAVEN</p>
     </a>
     <div class=" space-x-2">
